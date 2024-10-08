7eac8d8c8bfc92d0118c5f205690afcda062de78
Katsiaryna Tselishchava <16961074+tEkaterina@users.noreply.github.com>
2024-10-08 22:50:21 +0300

    feat: Add Application Layer Product.

---
+++ CatalogService/CatalogService.Application/Products/Commands/CreateProduct/CreateProductValidator.cs
@@ -0,0 +1,29 @@
+﻿using CatalogService.Application.Common.Extensions.Validators;
+using CatalogService.Application.Common.Interfaces;
+using FluentValidation;
+
+namespace CatalogService.Application.Products.Commands.CreateProduct
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductCommand>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public CreateProductValidator(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.Price).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.Amount).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.Description).ValidHtml();
+            RuleFor(x => x.Image).ValidUrl();
+            RuleFor(x => x.CategoryId).MustAsync(IsValidCategory).WithMessage("Invalid category ID.");
+        }
+
+        private async Task<bool> IsValidCategory(int categoryId, CancellationToken cancellationToken)
+        {
+            var category = await _dbContext.Categories.FindAsync([categoryId], cancellationToken);
+            return category != null;
+        }
+    }
+}
---
+++ CatalogService/CatalogService.Application/Products/Queries/Common/Dto/ProductDto.cs
@@ -0,0 +1,4 @@
+﻿namespace CatalogService.Application.Products.Queries.Common.Dto
+{
+    public record ProductDto(int Id, string Name, decimal Price, int Amount, int CategoryId, string? Description, string? Image);
+}
